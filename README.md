f9f52a173f10e041a7694052c4bf2773e12d29db
Jo Dudding <j.m.dudding@gmail.com>
2025-05-03 13:36:22 +1200

    update readme and tidy rcityviews script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-maps
+
+## [rcityviews](https://github.com/koenderks/rcityviews)
+
+![Pauanui](outputs/Pauanui-base.png)
--- scripts/rcityviews.r
+++ scripts/rcityviews.r
@@ -17,6 +17,8 @@ list_cities(match = 'Auckland')
 
 list_cities(match = 'Pauanui')
 
+#--- create custom for Pauanui to better center ---
+
 pauanui <- new_city(name = 'Pauanui',
   country = 'New Zealand',
   lat = -37.019,
@@ -36,7 +38,22 @@ gg_save <- function(save_name, plot = last_plot(), dpi = 100) {
   )
 }
 
-#--- create base map ---
+#--- pauanui maps ---
+
+p_pauanui  <- cityview(name = pauanui, zoom = 1.2, theme = 'vintage')
+p_pauanui2 <- cityview(name = pauanui, zoom = 1.2, theme = "rouge")
+p_pauanui3 <- cityview(name = pauanui, zoom = 1.2, theme = "bright")
+p_pauanui4 <- cityview(name = pauanui, zoom = 1.2, theme = "delftware")
+p_pauanui5 <- cityview(name = pauanui, zoom = 1.2, theme = "modern")
+
+gg_save('Pauanui-base', p_pauanui)
+gg_save('Pauanui-rouge', p_pauanui2)
+gg_save('Pauanui-bright', p_pauanui3)
+gg_save('Pauanui-delftware', p_pauanui4)
+gg_save('Pauanui-modern', p_pauanui5)
+
+
+#--- auckland maps ---
 
 p_auck <- cityview(name = 'Auckland', zoom = 1)
 
@@ -44,51 +61,10 @@ p_auck2 <- cityview(name = 'Auckland', zoom = 2)
 
 p_auck3 <- cityview(name = 'Auckland', zoom = 0.5)
 
-p_pauanui <- cityview(name = 'Pauanui Beach', zoom = 1)
-
-p_pauanui2 <- cityview(name = 'Pauanui Beach', zoom = 1.5, theme = "rouge")
-
-p_pauanui3 <- cityview(name = pauanui, zoom = 1.5, theme = "bright")
-
-p_pauanui4 <- cityview(name = pauanui, zoom = 1.5, theme = "delftware")
-
-p_pauanui5 <- cityview(name = pauanui, zoom = 1.5, theme = "modern")
-
-
-#--- save ---
-
-ggplot2::ggsave(
-  filename = "outputs/Auckland-base.png", 
-  plot = p_auck, 
-  height = 500, 
-  width = 500, 
-  units = "mm", 
-  dpi = 100
-)
-
-ggplot2::ggsave(
-  filename = "outputs/Auckland-base2.png", 
-  plot = p_auck2, 
-  height = 500, 
-  width = 500, 
-  units = "mm", 
-  dpi = 100
-)
-
-ggplot2::ggsave(
-  filename = "outputs/Auckland-base3.png", 
-  plot = p_auck3, 
-  height = 500, 
-  width = 500, 
-  units = "mm", 
-  dpi = 100
-)
+gg_save('Auckland-base', p_auck)
+gg_save('Auckland-base2', p_auck2)
+gg_save('Auckland-base3', p_auck3)
 
 
 
-gg_save('Pauanui-base', p_pauanui)
-gg_save('Pauanui-rouge.png', p_pauanui2)
-gg_save('Pauanui-bright', p_pauanui3)
-gg_save('Pauanui-delftware', p_pauanui4)
-gg_save('Pauanui-modern', p_pauanui5)
 
